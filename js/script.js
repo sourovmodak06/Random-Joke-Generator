e42473e52cc73e68d31872f3224de75670e9754f
sourovmodak06 <sourovmodak650@gmail.com>
2023-03-18 13:20:19 +0600

    Random Joke Generator done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log('hi');
+const loadApiData = async () => {
+    try {
+      const res = await fetch(
+        `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`
+      );
+      const data = await res.json();
+      randomJoke(data.joke);
+    } catch (error) {
+      alert(error);
+    }
+};
+
+const randomJoke = (joke)=> {
+    document.getElementById('joke-message').innerText = joke;
+}
+const newJoke = document.getElementById('joke');
+newJoke.addEventListener('click', () =>{
+    return loadApiData();
+})
+
+
+
+
+
+
+
+
+
+
+
